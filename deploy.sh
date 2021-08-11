c623e33947fb94dac3dfecc3aaf45df30e22e185
fengqian <397503856@qq.com>
2021-08-11 16:37:06 +0800

    name

--- deploy.sh
+++ deploy.sh
@@ -12,20 +12,19 @@ cd docs/.vuepress/dist
 # deploy to github pages
 # echo 'b.xugaoyi.com' > CNAME
 
-# if [ -z "$GITHUB_TOKEN" ]; then
-#   msg='deploy'
-#   githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-# else
-#   msg='来自github actions的自动部署'
-#   githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-#   git config --global user.name "xugaoyi"
-#   git config --global user.email "894072666@qq.com"
-# fi
-
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=https://github.com/gros-q/blog.git 
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://gros-q:${GITHUB_TOKEN}@github.com/gros-q/blog.git
+  git config --global user.name "gros-q"
+  git config --global user.email "397503856@qq.com"
+fi
 git init
 git add -A
-git commit -m "deploy"
-git push -f https://github.com/gros-q/blog.git master:gh-pages # 推送到github gh-pages分支
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
