f4abc2b161672164a17d8496efcffc34957f5335
fengqian <397503856@qq.com>
2021-11-17 10:45:02 +0800

    init

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -7,7 +7,7 @@ module.exports = {
   nav,
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: '/img/EB-logo.png', // 导航栏logo
-  repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+  // repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
@@ -47,8 +47,8 @@ module.exports = {
 
   author: {
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-    name: 'xugaoyi', // 必需
-    link: 'https://github.com/xugaoyi', // 可选的
+    name: 'gros-q', // 必需
+    link: 'https://github.com/gros-q', // 可选的
   },
   // blogger: {
   //   // 博主信息，显示在首页侧边栏
@@ -79,9 +79,9 @@ module.exports = {
   // },
   footer: {
     // 页脚信息
-    createYear: 2019, // 博客创建年份
+    createYear: 2021, // 博客创建年份
     copyrightInfo:
-      'fengqian', // 博客版权信息，支持a标签
-  },
-  htmlModules // 插入hmtl(广告)模块
+      'gros-q', // 博客版权信息，支持a标签
+  }
+  // htmlModules // 插入hmtl(广告)模块
 }
